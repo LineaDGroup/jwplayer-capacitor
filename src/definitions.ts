3de486dd7ad0307970b823ccdac105a1f87dbb77
Andres Diaz <andresdiaz@MacBook-Pro-de-Andres.local>
2021-09-29 22:41:34 -0500

    Add Google cast

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,6 +1,6 @@
 export interface JWPlayerPlugin {
   echo(options: { value: string }): Promise<{ value: string }>;
-  initialize(options: { androidLicenseKey: string, iosLicenseKey: string }): Promise<any>;
-  create(options: { videoURL: string, x: number, y:number, width: number, height: number }): Promise<any>;
+  initialize(options: { androidLicenseKey?: string, iosLicenseKey?: string , googleCastId?: string}): Promise<any>;
+  create(options: { videoURL: string, posterURL?: string,  forceFullScreenOnLandscape?: boolean, x: number, y:number, width: number, height: number }): Promise<any>;
   remove(): Promise<any>;
 }
